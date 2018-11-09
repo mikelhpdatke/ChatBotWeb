6257bbe569820f18baad74fa3358535a176c75b3
mikelhpdatke <mikelhpdatke@gmail.com>
2018-11-09 08:37:23 +0700

    first commit

---
+++ src/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import logo from './assets/ptit-logo.png'
+import { NavLink } from 'react-router-dom'
+const Menu = () => (
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <NavLink style={{ color: "black", fontWeight: "bold" }} className="nav-item nav-link" exact to='/' activeStyle={{
+                        fontWeight: "bold",
+                        color: "red",
+                        textDecoration: "underline"
+                    }}>Trang chủ </NavLink>
+                    <NavLink style={{ color: "black", fontWeight: "bold" }} className="nav-item nav-link" to='/user_management' activeStyle={{
+                        fontWeight: "bold",
+                        color: "red",
+                        textDecoration: "underline"
+                    }}>Thống kê</NavLink>
+                </div>
+
+            </div>
+);
+class Header extends Component{
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-expand-lg bg-light" fill variant="tabs" defaultActiveKey="/">
+                    <a class="navbar-brand" href="#">
+                        <img src={logo} width="120" height="60"></img>
+                    </a>
+                    
+                    <Menu/>
+                </nav>
+            </div>
+        );
+
+    }
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,211 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class AskAns extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ask: "aaa",
+      ans: "sss"
+    };
+    this.handleChange = this.handleChange.bind(this);
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    if (name === "ask" || name === "ans") {
+      this.setState({ [name]: event.target.value });
+      console.log(this.state.ans);
+    }
+  }
+
+  render() {
+    return (
+      <div class="col">
+        <div class="form-group">
+          <label for="comment" style={{ color: "black", fontWeight: "bold" }}>
+            Câu hỏi:
+          </label>
+          <textarea
+            name="ask"
+            class="form-control"
+            rows="5"
+            id="comment"
+            onChange={this.handleChange}
+          >
+            {this.state.ask}
+          </textarea>
+          <br />
+          <br />
+          <label for="comment" style={{ color: "black", fontWeight: "bold" }}>
+            Câu trả lời:
+          </label>
+          <textarea
+            name="ans"
+            class="form-control"
+            rows="5"
+            id="comment"
+            onChange={this.handleChange}
+          >
+            {this.state.ans}
+          </textarea>
+        </div>
+      </div>
+    );
+  }
+}
+
+const checkboxes = [
+  {
+    name: "check-box-1",
+    key: "checkBox1",
+    label: "Check Box 1"
+  },
+  {
+    name: "check-box-2",
+    key: "checkBox2",
+    label: "Check Box 2"
+  },
+  {
+    name: "check-box-3",
+    key: "checkBox3",
+    label: "Check Box 3"
+  },
+  {
+    name: "check-box-4",
+    key: "checkBox4",
+    label: "Check Box 4"
+  },
+  {
+    name: "check-box-1",
+    key: "checkBox1",
+    label: "Check Box 1"
+  },
+  {
+    name: "check-box-2",
+    key: "checkBox2",
+    label: "Check Box 2"
+  },
+  {
+    name: "check-box-3",
+    key: "checkBox3",
+    label: "Check Box 3"
+  },
+  {
+    name: "check-box-4",
+    key: "checkBox4",
+    label: "Check Box 4"
+  },
+  {
+    name: "check-box-4",
+    key: "checkBox4",
+    label: "Check Box 4"
+  },
+  {
+    name: "check-box-1",
+    key: "checkBox1",
+    label: "Check Box 1"
+  },
+  {
+    name: "check-box-2",
+    key: "checkBox2",
+    label: "Check Box 2"
+  },
+  {
+    name: "check-box-3",
+    key: "checkBox3",
+    label: "Check Box 3"
+  },
+  {
+    name: "check-box-4",
+    key: "checkBox4",
+    label: "Check Box 4"
+  }
+];
+
+const Checkbox = ({ type = "checkbox", name, checked = false, onChange }) => (
+  <input type={type} name={name} checked={checked} onChange={onChange} />
+);
+
+Checkbox.propTypes = {
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func.isRequired
+};
+
+class CheckboxContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checkedItems: new Map()
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const item = e.target.name;
+    const isChecked = e.target.checked;
+    this.setState(prevState => ({
+      checkedItems: prevState.checkedItems.set(item, isChecked)
+    }));
+  }
+
+  
+  render() {
+    let dem = 0;
+    return (
+      <div class="col mx-auto align-self-center justify-content-center text-center">
+        <div class="row">
+                {
+                    checkboxes.map( (item) => {
+                        //console.log(dem++);
+                        dem++;
+                        return (         
+                                
+                                    <div class="col-6" style={{height:45}}>      
+                                        <div class="checkbox">
+                                        <label key={item.key}>
+                                            <Checkbox
+                                            name={item.name}
+                                            checked={this.state.checkedItems.get(item.name)}
+                                            onChange={this.handleChange}
+                                            />
+                                            {item.name}
+                                        </label>
+                                        </div>
+                                    </div> 
+                        );
+                    }
+                )
+                }
+            </div>
+        </div>
+      
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="row justify-content-between">
+          <AskAns />
+          <CheckboxContainer/>
+        </div>
+        <br />
+        <div class="row justify-content-around">
+          <button type="button" class="btn btn-outline-success">
+            Submit
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
